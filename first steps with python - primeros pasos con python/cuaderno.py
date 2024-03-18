4072e075fe9b29721b4c3ca9f87cb2a64a1a78ce
pedroiha <Pedroiha99@user.noreply.github.com>
2024-03-18 11:32:31 -0600

    update notebook

--- first steps with python - primeros pasos con python/cuaderno.py
+++ first steps with python - primeros pasos con python/cuaderno.py
@@ -6,6 +6,37 @@ print("hello world.")
 #Variables, expressions and statements.
 #Values and types.
 
-#We can know what type of variable it is with the following function.
+#We can know what type of variable it is with the following function.   
+
+msj = "hello world"
+type(msj) #You must remember that we have to print the result.
+print(type(msj))
+
+#variable types.
+
+a = 5
+b = 11.5
+msgs = "hi my friend"
+
+print(type(a)) #integer type
+print(type(b)) #type float
+print(type(msgs)) #string type
+
+#Python has 28 reserved words:
+"""
+and         continue       else        for         import          not         raise
+assert      def            except      from        in              or          return
+break       del            exec        global      is              pass        try
+class       elif           finally     if          lambda          print       while
+"""
+
+#El orden de las operaciones
+"""Las operaciones tiene un orden de ejecucion. Primero son las operaciones que estan estre parentesis, despues la exponenciacion,
+#multiplicacion y division. Los operadores se evaluan de izquierda a derecha."""
+
+
+
+
+
 
 
